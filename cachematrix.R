c6f24785dfaeb7ec0b4f7234944773d99bf4daf3
afanay01 <elena27@gmail.com>
2015-08-22 23:21:42 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to cache inverse of a matrix
 
-## Write a short comment describing this function
+## Create a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = numeric()) {
   m <- NULL
